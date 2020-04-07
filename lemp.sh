e5cec86c0dfabeea63c0c49ec702af3d26cb81bd
flareblare <49785717+flareblare@users.noreply.github.com>
2020-04-07 15:45:17 +0200

    Update lemp.sh
    -Y

--- lemp.sh
+++ lemp.sh
@@ -7,10 +7,10 @@ EOL'
 wget http://nginx.org/keys/nginx_signing.key
 sudo apt-key add nginx_signing.key
 sudo apt-get update
-sudo apt-get install nginx
+sudo apt-get install nginx -Y
 
 # Install MariaDB
-sudo apt-get install mariadb-server # Or MySQL: sudo apt-get install mysql-server
+sudo apt-get install mariadb-server -Y # Or MySQL: sudo apt-get install mysql-server
 sudo service mysql stop # Stop the MySQL if is running.
 #sudo mysql_install_db
 #sudo service mysql start
@@ -19,7 +19,7 @@ sudo service mysql stop # Stop the MySQL if is running.
 # Install PHP 7.4
 sudo add-apt-repository ppa:ondrej/php
 sudo apt-get update
-sudo apt-get install php7.4
+sudo apt-get install php7.4 -Y
 
 # Install needed modules for PHP
 sudo apt-get install php7.4-fpm php7.4-mysql php7.4-curl php7.4-gd php7.4-bz2 php7.4-mbstring php7.4-xml php7.4-zip
